7a963230d5baa93dde6f23e748229984064ef400
tharinda <tharinda221@gmail.com>
2016-05-22 15:17:53 +0530

    initial commit

---
+++ simple-flask-web-application.py
@@ -0,0 +1,12 @@
+from flask import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(host="localhost", port=5000)
